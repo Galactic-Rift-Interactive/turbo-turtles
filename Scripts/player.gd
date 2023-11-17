036d5380e228f7bd388af7eee2514590ec15baa0
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2023-11-16 21:37:23 -0500

    Renaming files

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,10 @@
 # Jordan's GLORIOUS turtle car script
-
 extends CharacterBody3D
-
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MAX_SPEED = 100.0
-const ACCELERATION = 2.0
+const ACCELERATION = 5.0
 const ROTATION_SPEED = 2.0
 var current_speed = 0.0
 var animation_tree : AnimationTree
@@ -23,7 +21,7 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle Jump.
+	# Handle Jump.Al
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
